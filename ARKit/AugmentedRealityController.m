6d5fde86f5a0b8623f547bc55d8d77824e255610
Christian Salvaneschi <chrjs75@gmail.com>
2014-05-21 08:46:30 +0200

    Increase of the total vertical span of the y position of the markers, that is proportional to the distance.

--- ARKit/AugmentedRealityController.m
+++ ARKit/AugmentedRealityController.m
@@ -471,7 +471,7 @@
 	
     float radialDistanceKm = coordinate.radialDistance / 1000;
     float yFactor = radialDistanceKm / self.radarRange;
-    float ySpan = realityBounds.size.height / 5;
+    float ySpan = realityBounds.size.height / 3;
 	//point.y = (realityBounds.size.height / 2) + (radianToDegrees(M_PI_2 + viewAngle)  * 2.0);
   	point.y = (realityBounds.size.height / 2) - (ySpan / 2) + yFactor * ySpan;
     
